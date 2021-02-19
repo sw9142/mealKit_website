e68bf9f24198d2fe9faa598846923ca6edb3feee
sw9142 <sw9142@gmail.com>
2021-02-19 16:53:31 -0500

    created validation for registration page

--- controllers/general.js
+++ controllers/general.js
@@ -25,7 +25,57 @@ router.get("/reg", (req, res) => {
   res.render("general/registration");
 });
 
+router.post("/reg", (req, res) => {
+  let isValidated = true;
+  let messageValidation = {};
+  let regEpx_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; // retrieved from https://regexr.com/3e48o
+  let regEpx_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/; //retrieved from https://regexr.com/3bfsi
+  console.log(req.body);
+
+  const { fname, lname, email, password } = req.body;
+
+  if (typeof fname !== "string" || fname.length === 0) {
+    isValidated = false;
+    messageValidation.fname = "Please enter your first name :)";
+  } else if (typeof lname !== "string" || lname.length === 0) {
+    isValidated = false;
+    messageValidation.lname = "Please enter your last name :)";
+  } else if (email.length === 0) {
+    isValidated = false;
+    messageValidation.email = "Please enter your email :)";
+  } else if (password.length === 0) {
+    isValidated = false;
+    messageValidation.password = "Please enter the password";
+  } else if (fname.length <= 2) {
+    isValidated = false;
+    messageValidation.fname = "Please let us know your full first name :)";
+  } else if (lname.length <= 2) {
+    isValidated = false;
+    messageValidation.lname = "Please let us know your full last name :)";
+  } else if (!regEpx_email.test(email)) {
+    isValidated = false;
+    messageValidation.email = "Please enter your email correctly";
+  } else if (!regEpx_password.test(password)) {
+    isValidated = false;
+    messageValidation.password =
+      "Password must be more than 8 characters and contain at least one uppercase and one number digit";
+  }
+
+  if (isValidated) {
+    res.send("Success! :)");
+  } else {
+    res.render("general/registration", {
+      value: req.body,
+      messageValidation: messageValidation,
+    });
+  }
+});
+
 router.get("/login", (req, res) => {
+  res.render("general/login");
+});
+
+router.post("/login", (req, res) => {
   res.render("general/login");
 });
 
